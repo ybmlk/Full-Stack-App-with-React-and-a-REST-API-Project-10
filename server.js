91c03b346a1160f4d9c874f3210c784d09292f35
Yonatan Melka <ybmelka@gmail.com>
2020-03-04 20:30:01 -0600

    Update deploy

--- server.js
+++ server.js
@@ -40,9 +40,9 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('/client/build'));
 
   // load index.html
-  app.get('*', (req, res) => {
+  /* app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
+  }); */
 }
 
 // listen to port
