1174c2b2124dc8ec49fe1d11a6f83c3b260368e1
Yonatan Melka <ybmelka@gmail.com>
2020-03-06 15:47:48 -0600

    Add hooks to DeleteCourse

--- client/src/components/DeleteCourse.js
+++ client/src/components/DeleteCourse.js
@@ -1,73 +1,56 @@
-import React, { Component } from 'react';
+import React, { Component, useEffect, useState, memo } from 'react';
 import { Link } from 'react-router-dom';
 
-class DeleteCourse extends Component {
-  constructor({ context, match }) {
-    super();
-    this.context = context;
-    this.match = match;
-    this.data = this.context.data;
-    this.authUser = this.context.authenticatedUser;
-  }
+const DeleteCourse = ({ context: { data, authenticatedUser }, match: { params }, history }) => {
+  const [course, setCourse] = useState({});
 
-  state = {
-    course: {},
-  };
-
-  componentDidMount() {
-    const { id } = this.match.params;
+  useEffect(() => {
     // Retrieves a courses with a given 'id'
-    this.data
-      .getCourse(id)
+    data
+      .getCourse(params.id)
       .then(async res => {
         if (res.status === 200) {
           const course = await res.json();
-          this.setState(() => ({
-            course: course,
-          }));
+          setCourse({ course });
         } else {
-          this.props.history.push('/notfound');
+          history.push('/notfound');
         }
       })
       .catch(err => {
         console.log(err);
-        this.props.history.push('/error');
+        history.push('/error');
       });
-  }
+  }, []);
 
-  handleDelete = () => {
-    const { id } = this.match.params;
-    const { emailAddress, password } = this.authUser;
-    this.data
+  const handleDelete = () => {
+    const { emailAddress, password } = authenticatedUser;
+    data
       /* Passes the course's Id, the currently authenticated user's (who is also the owner)
       username and password as an argument to delete the course */
-      .deleteCourse(id, emailAddress, password)
+      .deleteCourse(params.id, emailAddress, password)
       .then(res => {
         if (res.status === 204) {
-          this.props.history.push('/');
+          history.push('/');
         } else {
           throw new Error();
         }
       })
       .catch(err => {
         console.log(err);
-        this.props.history.push('/error');
+        history.push('/error');
       });
   };
 
-  render() {
-    const { id } = this.match.params;
-    return (
+  return (
+    <div className='bounds course--detail'>
+      <h1>Are you sure, you want to delete this course?</h1>
+      <div className='input-title course--title--input'>Course: {course.title}</div>
       <div className='bounds course--detail'>
-        <h1>Are you sure, you want to delete this course?</h1>
-        <div className='input-title course--title--input'>Course: {this.state.course.title}</div>
-        <div className='bounds course--detail'>
-          <Bottom id={id} handleDelete={this.handleDelete} />
-        </div>
+        <Bottom id={params.id} handleDelete={handleDelete} />
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 const Bottom = ({ id, handleDelete }) => (
   <div className='grid-100 pad-bottom'>
@@ -80,4 +63,4 @@ const Bottom = ({ id, handleDelete }) => (
   </div>
 );
 
-export default DeleteCourse;
+export default memo(DeleteCourse);
