3123ef47972f652ab517316d0722d5f9b6c054e3
Yonatan Melka <ybmelka@gmail.com>
2020-03-06 21:34:24 -0600

    Add hook to AuthOwner

--- client/src/AuthOwner.js
+++ client/src/AuthOwner.js
@@ -1,65 +1,54 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { Route, Redirect } from 'react-router-dom';
+import { Context } from './Context';
+import useGetCourse from './hooks/useGetCourse';
 
 /* This component restricts some private routes like '/update' and '/delete'
 to only be accessed by the creators of the course */
-class AuthOwner extends Component {
-  state = {
-    user: {},
-  };
+const AuthOwner = ({ component: Component, id, history, ...rest }) => {
+  // State declaration
+  const [user, setUser] = useState({});
 
-  componentDidMount() {
-    const { context, id, history } = this.props;
-    const { data } = context;
+  // Import 'data' class from context
+  const { authenticatedUser } = useContext(Context);
 
-    // Retrieves a course with a given 'Id'
-    data
-      .getCourse(id)
-      .then(async res => {
-        if (res.status === 200) {
-          const course = await res.json();
-          // Stores the owner of that course inside state
-          this.setState(() => ({
-            user: course.user,
-          }));
-        } else {
-          history.push('/notfound');
-        }
-      })
-      .catch(err => {
-        console.log(err);
-        history.push('/error');
-      });
-  }
-  render() {
-    // `component: Component` means import `component as Component`
-    const { component: Component, id, context, ...rest } = this.props;
-    // The owner of the course
-    const owner = this.state.user._id;
-    // The authenticated user
-    const authUser = context.authenticatedUser._id;
+  // Retrieves a course with a given 'Id'
+  const [, fetchedUser] = useGetCourse(id, history);
 
-    return (
-      <Route
-        {...rest}
-        render={props => {
-          if (owner !== undefined) {
-            // If the authenticated user is the owner render the component
-            if (authUser === owner) {
-              return <Component {...props} />;
-            } else {
-              // Else redirect to '/forbidden' and store this path in 'props.location.state'
-              // B/c when the user is eventually authenticated they will be redirected to the path which they originally attempted to access
-              return <Redirect to={{
-                    pathname: '/forbidden',
-                    state: { fromForbidden: props.location },
-                  }} />;
-            }
+  useEffect(() => {
+    setUser(fetchedUser);
+  }, [fetchedUser]);
+
+
+  // The owner of the course
+  const owner = user._id;
+  // The authenticated user
+  const authUser = authenticatedUser._id;
+
+  return (
+    <Route
+      {...rest}
+      render={props => {
+        if (owner !== undefined) {
+          // If the authenticated user is the owner render the component
+          if (authUser === owner) {
+            return <Component {...props} />;
+          } else {
+            // Else redirect to '/forbidden' and store this path in 'props.location.state'
+            // B/c when the user is eventually authenticated they will be redirected to the path which they originally attempted to access
+            return (
+              <Redirect
+                to={{
+                  pathname: '/forbidden',
+                  state: { fromForbidden: props.location },
+                }}
+              />
+            );
           }
-        }}
-      />
-    );
-  }
-}
+        }
+      }}
+    />
+  );
+};
 
 export default AuthOwner;
